2e995d7df0c1cf4f5f83d2a7a859bc422cde594f
Kelly Vernon <kvernon@techstyle.com>
2021-03-15 08:31:59 -0500

    added jest for converage in lib

---
+++ libs/testing/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  displayName: 'testing',
+  preset: '../../jest.preset.js',
+  projects: [
+    '<rootDir>/libs/testing',
+    '<rootDir>/libs/testing-test',
+  ],
+  testEnvironment: 'node'
+};
